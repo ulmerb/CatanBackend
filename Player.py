f74d11beaa4fb60d93ba23b12488e1658199a96c
benlimon@stanford.edu <benlimon@stanford.edu>
2016-04-27 20:54:05 -0700

    cardName added

--- Player.py
+++ Player.py
@@ -109,6 +109,7 @@ class player:
 	def buildDevCard(self):
 		if self.canPlayDevCard:
 		    #we will need a deck to draw from
+		    cardName
 		    self.devCardsHeld.append('dummy')
 		    self.resources['wool'] -= 1
 		    self.resources['grain'] -= 1
