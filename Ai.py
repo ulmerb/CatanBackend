d789829095ef5f0a4709a9c040c697b34acaad41
David Beam <david@simpleemotion.com>
2016-05-06 15:43:16 -0700

    added matts functions to ai scaffolding

--- Ai.py
+++ Ai.py
@@ -21,9 +21,51 @@ class ai:
     def updateBuildLocations(self):
         pass
         
-    def evaluateLocationCost(self):
-        pass
-        
+    # getResourceCost(buildType,roadsAway)
+    # Varible one, build type, "road", "settlement", "city", "devCard"
+    # Variable two, number of roads away
+    # 
+    def getResourceCost(self, buildType,roadsAway):
+   	cost =  {'wood':0, 'sheep':0, 'brick': 0, 'ore': 0, 'grain' : 0}
+   	if(buildType == "road"):
+  		cost['brick'] += 1
+  		cost['wood'] += 1
+   	elif (buildType == "settlement"):
+  		cost['sheep'] += 1
+  		cost['brick'] += 1
+  		cost['wood'] += 1
+  		cost['grain'] += 1
+  		cost['brick'] += roadsAway
+  		cost['wood'] += roadsAway
+   	elif (buildType == "city"):
+  		cost['grain'] += 2
+  		cost['ore'] += 3
+   	elif (buildType == "devCard"):
+  		cost['sheep'] += 1
+  		cost['grain'] += 1
+  		cost['ore'] += 1
+   	else:
+  		print "invalid getResourceCost() call",buildType,roadsAway
+   	
+   	return cost
+    
+    def getVictoryPoints(self):
+   	return self.AI.getScore();
+    
+    def getCostInTurns(self, buildType,roadsAway,incomeMap):
+   	resCost = self.getResourceCost(buildType,roadsAway)
+   	turnCost = 0.0 
+   	for key in resCost.keys():
+  		curIncome = incomeMap[key]
+  		curCost = resCost[key]
+  		if (curIncome == 0):
+ 			return -1 #sentinel
+  		else:
+ 			val = curCost / curIncome
+ 			if (val > turnCost):
+    				turnCost = val
+   	return turnCost
+            
     def evaluateLocationBenefit(self, vert):
         tiles = self.board.getVertexToTiles(vert)
         exReturn =  {'wood':0.0, 'sheep':0.0, 'brick': 0.0, 'ore': 0.0, 'grain' : 0.0}
@@ -33,9 +75,7 @@ class ai:
                 continue 
             exReturn[tileType] += self.diceProbs[tile.getNumber() - 2]
         return exReturn
-        
-        
-        
+             
     def decideMove(self):
         pass
                
