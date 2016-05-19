8faf9afe7655c0b25377849580a2f231aac538f9
Mark Schramm <mschramm@stanford.edu>
2016-05-18 20:54:19 -0700

    integrated communication solutions infratrode

--- CatanApp/CatanApp/views.py
+++ CatanApp/CatanApp/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 @csrf_exempt
 def djangotest(request):
-	print request.POST
-	return HttpResponse("somestring")
+	z = json.loads(request.POST['js_resp'])
+	print z['action']
+	return HttpResponse("Hello")
