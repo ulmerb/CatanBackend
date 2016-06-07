c698c1339cfa9aa790c81c54711f0f5443bff9bf
Matt Fernandez <matt@rescomp-15-290241.stanford.edu>
2016-06-06 20:37:22 -0700

    fixing ascii settlement/city bugs

--- CatanApp/CatanApp/Controller.py
+++ CatanApp/CatanApp/Controller.py
@@ -451,9 +451,9 @@ def firstPlacement(numPlayers, players, board, AiNum = -1):
 		if (i == AiNum):
 			#print i
 			players[AiNum].decideMove(players, board, True)
-			#board.createBatchCSV(players)
-			#board.batchUpdate()
-			#print board.printBoard()
+			board.createBatchCSV(players)
+			board.batchUpdate()
+			print board.printBoard()
 			continue
 		print board.printBoard()    
 		initialPlacement(i, players, board)
@@ -463,9 +463,9 @@ def firstPlacement(numPlayers, players, board, AiNum = -1):
 	for i in range(numPlayers -1, -1, -1):
 		if (i == AiNum):
 			players[AiNum].decideMove(players, board, True)
-			#board.createBatchCSV(players)
-			#board.batchUpdate()
-			#print board.printBoard()
+			board.createBatchCSV(players)
+			board.batchUpdate()
+			print board.printBoard()
 			continue
 		print board.printBoard()
 	 	setLoc = initialPlacement(i, players, board)
@@ -833,5 +833,5 @@ def isInt(s):
         return False
 
 # comment out main when using controller to handle requests
-#main()
+main()
 
